71f4fb127ae2e5fde0c43d55e2980b7587465e69
Eliot Swank <eliotswank@gmail.com>
2018-07-08 14:49:51 -0600

    adds sees all categories on index page test

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -27,8 +27,6 @@ end
 
 
 =begin
- When the user tries to create a Category that already exists, they are brought back to the page with the form to create a Category.
- The user can view a list of all Categories on a single page, and each Category can be deleted from that page. There is also a link to “Edit” each Category, which takes the user to a form where they can update the Category.
  When the user creates a new Job, they are required to select its Category from a drop down menu of existing categories. They also see a link to create a new Category.
  When the user visits a page for a specific Category, they see a list of Jobs in that Category.
 =end
