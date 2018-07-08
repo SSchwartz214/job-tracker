ed125fb81e47f76428a17ee7d9bdc5a2c3732758
Eliot Swank <eliotswank@gmail.com>
2018-07-08 14:33:26 -0600

    controller refactoring

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,20 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -30,41 +22,34 @@ class CategoriesController < ApplicationController
       redirect_to category_path(@category)
     else
       flash.now[:error] = "Category must be unique."
-      render :new
+      redirect_to new_category_path
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    if @company.save
+      flash[:success] = "#{@company.name} updated!"
+      redirect_to company_path(@company)
+    else
+      render :edit
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: 'Category was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
       params.require(:category).permit(:title)
     end
