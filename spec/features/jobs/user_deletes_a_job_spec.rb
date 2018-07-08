2cfc27964abf6907f1dcf28771a48711c34ffd11
Seth Schwartz <sschwartz214@gmail.com>
2018-07-08 16:21:53 -0600

    adds category id to job

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "User deletes exisiting job" do
   scenario "a user can delete a job" do
     company = Company.create(name: "ESPN")
-    job = company.jobs.create(title: "Dev", description: "fun", level_of_interest: 30, city: "Chicago")
+    category = Category.create(title: "Web Dev")
+    job = company.jobs.create(title: "Dev", description: "fun", level_of_interest: 30, city: "Chicago", category_id: category.id)
 
     visit company_jobs_path(company)
 
--- spec/features/jobs/user_edits_existing_job_spec.rb
+++ spec/features/jobs/user_edits_existing_job_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "User edits and existing job" do
   scenario "a user can edit a job" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 80, city: "Denver")
+    category = Category.create(title: "Web Dev")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 80, city: "Denver", category_id: category.id)
 
     visit edit_company_job_path(company, job)
 
