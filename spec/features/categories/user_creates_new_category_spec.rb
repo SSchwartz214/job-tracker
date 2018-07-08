488f445197f15eb7f258a7df32c716d7e20edd7d
Eliot Swank <eliotswank@gmail.com>
2018-07-08 11:42:25 -0600

    adds test for dupliucate category entry

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -11,14 +11,22 @@ describe "user visits /categories/new" do
       expect(current_path).to eq("/categories/#{Category.all.first.id}")
       expect(page).to have_content("Web Development")
     end
+    it "redirects to the new page if they try to create a duplicate category" do
+      category = Category.create(title:"Web Development")
+
+      visit(new_category_path)
+
+      fill_in "category[title]",	with: "Web Development"
+      click_on "Create Category"
+
+      expect(current_path).to eq(new_category_path)
+    end
   end
 end
 
 
 
 =begin
-The user can create a new Category by filling out a form. Each Category has a title (e.g. “Web Development”, “Education”, “Finance”).
- When the user successfully creates a Category they are shown a page with the Category title.
  When the user tries to create a Category that already exists, they are brought back to the page with the form to create a Category.
  The user can view a list of all Categories on a single page, and each Category can be deleted from that page. There is also a link to “Edit” each Category, which takes the user to a form where they can update the Category.
  When the user creates a new Job, they are required to select its Category from a drop down menu of existing categories. They also see a link to create a new Category.
