494f92d93c2293e212a962c4f29869fb184b5d88
Eliot Swank <eliotswank@gmail.com>
2018-07-08 14:53:20 -0600

    removes unneccessary files

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validations' do
-    describe 'is invalid without a title and title must be unique' do
       it { should validate_presence_of(:title) }
-    end
-    describe 'and title must be unique' do
-      xit { should validate_uniqueness_of(:title) }
-    end
+      it { should validate_uniqueness_of(:title) }
   end
 end
