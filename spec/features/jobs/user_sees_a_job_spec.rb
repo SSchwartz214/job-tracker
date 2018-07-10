782af3799c2399d1d0ad3d2c95108edc3e01dde4
Seth Schwartz <sschwartz214@gmail.com>
2018-07-09 18:48:48 -0600

    adds test for adding comment

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -20,3 +20,21 @@ describe "User sees a specific job" do
 
   end
 end
+  describe "they fill in a comment form" do
+    it "displays the comment on the job show" do
+      company = Company.create!(name: "ESPN")
+      category = Category.create(title: "Baz")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+
+      visit company_job_path(company, job)
+
+      fill_in "comment[author_name]", with: "Me"
+      fill_in "comment[body]", with: "Sweet job"
+      click_on "Submit"
+
+      expect(curent_path).to eq(company_job_path(company, job))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("Me")
+      expect(page).to have_content("Sweet job")
+    end
+  end
