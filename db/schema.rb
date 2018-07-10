d8d8daffbf4c134088a7ce04b56c8eb5f9159083
Seth Schwartz <sschwartz214@gmail.com>
2018-07-10 12:15:03 -0600

    adds migration for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180709033838) do
+ActiveRecord::Schema.define(version: 20180710174004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20180709033838) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "dashboards", force: :cascade do |t|
   end
 
   create_table "jobs", force: :cascade do |t|
