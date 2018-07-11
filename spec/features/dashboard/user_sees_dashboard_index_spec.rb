3b2d0a6f568d0214b3e604f69763287b3221c775
Eliot Swank <eliotswank@gmail.com>
2018-07-11 11:39:06 -0600

    writes feature test for jobs by location

--- spec/features/dashboard/user_sees_dashboard_index_spec.rb
+++ spec/features/dashboard/user_sees_dashboard_index_spec.rb
@@ -12,7 +12,7 @@ describe "user visits dashboard" do
     expect(page).to have_content("Level of Interest: 70 (1)")
     expect(page).to have_content("Level of Interest: 50 (1)")
   end
-  
+
   it "sees the top three companies ranked by average level of interest along with their respective average level of interest" do
     company1 = Company.create(name: "hahaha")
     company2 = Company.create(name: "company2")
@@ -37,5 +37,24 @@ describe "user visits dashboard" do
     expect(page).to have_content(expected1)
     expect(page).to have_content(expected2)
     expect(page).to have_content(expected3)
+  end
+
+  it 'has a count of jobs by location' do
+    company1 = Company.create(name: "hahaha")
+    company2 = Company.create(name: "company2")
+    company3 = Company.create(name: "company3")
+    category = Category.create(title: "the best")
+    Job.create!(title: "foo1", description: "bar", level_of_interest: 30, city: "NY", company_id: company1.id, category_id: category.id)
+    Job.create!(title: "baz1", description: "bar", level_of_interest: 40, city: "LA", company_id: company1.id, category_id: category.id)
+    Job.create!(title: "foo2", description: "bar", level_of_interest: 50, city: "NY", company_id: company2.id, category_id: category.id)
+    Job.create!(title: "baz2", description: "bar", level_of_interest: 60, city: "LA", company_id: company2.id, category_id: category.id)
+    Job.create!(title: "foo3", description: "bar", level_of_interest: 70, city: "NY", company_id: company3.id, category_id: category.id)
+    Job.create!(title: "baz3", description: "bar", level_of_interest: 80, city: "LA", company_id: company3.id, category_id: category.id)
+
+    visit dashboard_index_path
+
+    expect(page).to have_content("Jobs by Location")
+    expect(page).to have_content("NY - 3")
+    expect(page).to have_content("LA - 3")
   end
 end
